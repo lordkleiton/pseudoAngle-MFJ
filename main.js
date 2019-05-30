5bf540774314991ce0aeec07625720e25347fcae
lord <m_s_r@outlook.com>
2019-05-30 12:51:29 -0300

    feito - desenhando angulos

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let maxWH = 300;
 let padding = (canvas.clientWidth - maxWH) / 2
 let min = centerX - (maxWH / 2)
 let max = centerX + (maxWH / 2)
+let q = 0
 
 canvas.addEventListener('mousemove', drawToMouse)
 ctx.fillStyle = 'black'
@@ -39,35 +40,131 @@ function drawToMouse(e){
     if (teste2 < 0) teste2 = 0
     if (teste2 > 1) teste2 = 1
 
-    let ax = (teste2 > 0.5) ? 'positivo' : 'negativo'
-    let ay = (teste < 0.5) ? 'positivo' : 'negativo'    
-
     let c = teste2 * 2 - 1  //intervalo -1, 1
     let d = 1 - teste * 2   //intervalo 1, -1
 
-    if (c > 0 && d > 0 && c > d) console.log('quadrante 1')
-    if (c > 0 && d > 0 && c < d) console.log('quadrante 2')
-    if (c < 0 && d > 0 && -c < d) console.log('quadrante 3')
-    if (c < 0 && d > 0 && -c > d) console.log('quadrante 4')
-    if (c < 0 && d < 0 && c < d) console.log('quadrante 5')
-    if (c < 0 && d < 0 && c > d) console.log('quadrante 6')
-    if (c > 0 && d < 0 && c < -d) console.log('quadrante 7')
-    if (c > 0 && d < 0 && c > -d) console.log('quadrante 8')
+    if (c > 0 && d > 0 && c > d) q = 1
+    if (c > 0 && d > 0 && c < d) q = 2
+    if (c < 0 && d > 0 && -c < d) q = 3
+    if (c < 0 && d > 0 && -c > d) q = 4
+    if (c < 0 && d < 0 && c < d) q = 5
+    if (c < 0 && d < 0 && c > d) q = 6
+    if (c > 0 && d < 0 && c < -d) q = 7
+    if (c > 0 && d < 0 && c > -d) q = 8
 
-
-    console.log('x: ', c, 'y: ', d)
-
-    drawAngle(teste2 * maxWH + padding, teste * maxWH + padding)
+    drawAngle(teste2 * maxWH + padding, teste * maxWH + padding, q, (q % 2 === 0))
 }
 
-function drawAngle(x, y){
+function drawAngle(x, y, q, c){
     ctx.strokeStyle = 'red'
-    ctx.beginPath()
-    ctx.moveTo(400, centerY)
-    ctx.lineTo(400, y)
-    ctx.lineTo(x, y)
+
+    if (q === 1){
+        if (!c){
+            ctx.beginPath()
+            ctx.moveTo(400, centerY)
+            ctx.lineTo(400, y)
+        }
+        else{
+            ctx.beginPath()
+            ctx.moveTo(max, centerY)
+            ctx.lineTo(max, min)
+        }
+    }
+    if (q > 1) {
+        drawAngle(x, y, 1, true)
+
+        if (c){
+            ctx.beginPath()
+            ctx.moveTo(max, min)
+            ctx.lineTo(x, min)
+        }
+        else{
+            ctx.beginPath()
+            ctx.moveTo(max, min)
+            ctx.lineTo(centerX, min)
+        }
+    }
+    if (q > 2) {
+        drawAngle(x, y, 2, false)
+
+        if (!c){
+            ctx.beginPath()
+            ctx.moveTo(max, min)
+            ctx.lineTo(x, min)
+        }
+        else{
+            ctx.beginPath()
+            ctx.moveTo(centerX, min)
+            ctx.lineTo(min, min)
+        }
+    }
+    if (q > 3) {
+        drawAngle(x, y, 3, true)
+
+        if (c) {
+            ctx.beginPath()
+            ctx.moveTo(min, min)
+            ctx.lineTo(min, y)
+        }
+        else {
+            ctx.beginPath()
+            ctx.moveTo(min, min)
+            ctx.lineTo(min, centerY)
+        }
+    }
+    if (q > 4) {
+        drawAngle(x, y, 4, true)
+
+        if (!c) {
+            ctx.beginPath()
+            ctx.moveTo(min, centerY)
+            ctx.lineTo(min, y)
+        }
+        else {
+            ctx.beginPath()
+            ctx.moveTo(min, centerY)
+            ctx.lineTo(min, max)
+        }
+    }
+    if (q > 5) {
+        drawAngle(x, y, 5, true)
+
+        if (c) {
+            ctx.beginPath()
+            ctx.moveTo(min, max)
+            ctx.lineTo(x, max)
+        }
+        else {
+            ctx.beginPath()
+            ctx.moveTo(min, max)
+            ctx.lineTo(centerX, max)
+        }
+    }
+    if (q > 6) {
+        drawAngle(x, y, 6, true)
+
+        if (!c) {
+            ctx.beginPath()
+            ctx.moveTo(centerX, max)
+            ctx.lineTo(x, max)
+        }
+        else {
+            ctx.beginPath()
+            ctx.moveTo(centerX, max)
+            ctx.lineTo(max, max)
+        }
+    }
+    if (q > 7) {
+        drawAngle(x, y, 7, true)
+
+        if (c) {
+            ctx.beginPath()
+            ctx.moveTo(max, max)
+            ctx.lineTo(max, y)
+        }
+    }
+
     ctx.stroke()
-    ctx.strokeStyle = '#ccc'
 }
 
 function drawGrid(){
