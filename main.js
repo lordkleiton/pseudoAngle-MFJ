790ba1be7005928f93e031ecdb2f295b2c1731f6
lord <m_s_r@outlook.com>
2019-05-30 11:00:08 -0300

    intervalos 1, -1

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let ctx = canvas.getContext('2d')
 let centerX = canvas.clientWidth / 2
 let centerY = canvas.clientHeight / 2
 let maxWH = 300;
+let padding = (canvas.clientWidth - maxWH) / 2
 let min = centerX - (maxWH / 2)
 let max = centerX + (maxWH / 2)
 
@@ -13,14 +14,13 @@ ctx.fillStyle = 'black'
 
 drawGrid()
 
-function clearCanvas(){									//limpa o canvas
+function clearCanvas(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.beginPath();
 }
 
 function drawToMouse(e){
     redraw()
-
 
     let y = e.clientY
     let x = e.clientX
@@ -31,17 +31,23 @@ function drawToMouse(e){
     ctx.lineTo(x, y)
     ctx.stroke()
 
-    let teste = (y-100)/300
+    let teste = (y - padding) / maxWH
     if (teste < 0) teste = 0
     if (teste > 1) teste = 1
 
-    let teste2 = (x-100)/300
+    let teste2 = (x - padding) / maxWH
     if (teste2 < 0) teste2 = 0
     if (teste2 > 1) teste2 = 1
 
-    console.log(teste*300, teste2*300)
+    let ax = (teste2 > 0.5) ? 'positivo' : 'negativo'
+    let ay = (teste < 0.5) ? 'positivo' : 'negativo'    
 
-    drawAngle(teste2*300+100, teste*300+100)
+    let c = teste2 * 2 - 1
+    let d = 1 - teste * 2
+
+    console.log('x: ', c, 'y: ', d)
+
+    drawAngle(teste2 * maxWH + padding, teste * maxWH + padding)
 }
 
 function drawAngle(x, y){
